96c03e3679fb323c0ff56421ef5fc6f75f6a77c2
NAVEEN K <naveenkkn03@gmail.com>
2024-01-20 00:33:21 +0530

    config changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-  // server:{
-  //   proxy:{
-  //     '/api':{
-  //       target:'http://localhost:3005',
-  //     }
-  //   }
-  // },
-  // optimizeDeps: {
-  //   exclude: ['js-big-decimal']
-  // }
 })
